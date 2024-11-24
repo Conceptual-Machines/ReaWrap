f16dc3ea71ddea1c71fd6784c84e3c56df7ee163
lucaromagnoli <romagnoli.luca@gmail.com>
2024-11-24 17:31:37 +0000

    fixes to helpers

--- modules/helpers.lua
+++ modules/helpers.lua
@@ -6,6 +6,8 @@
 
 local r = reaper
 
+-- @class helpers
+-- Helper functions for common tasks.
 local helpers = {}
 
 --- Return a function that joins a variable number of arguments, separated by the argument `sep` (default ', ').
